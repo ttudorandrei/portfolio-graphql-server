1c31c55e7e2c91e0300958116c41608bdbb5e035
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-14 18:17:08 +0100

    add repos and user resolvers

--- src/resolvers/repos.js
+++ src/resolvers/repos.js
@@ -1,3 +1,15 @@
-const repos = () => {};
+const repos = () => {
+  const data = await fetchDataFromGithub(
+    "https://api.github.com/users/ttudorandrei/repos"
+  );
+
+  console.log(data);
+
+  return {
+    repoName: data.name,
+    repoUrl: data.html_url,
+    createdAt: data.created_at,
+  };
+};
 
 module.exports = repos;
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,3 +1,27 @@
-const user = () => {};
+const { fetchDataFromGithub } = require("../utils/fetchers/github");
+
+const user = async () => {
+  const data = await fetchDataFromGithub(
+    "https://api.github.com/users/ttudorandrei"
+  );
+
+  console.log(data);
+
+  return {
+    username: data.login,
+    profilePictureUrl: data.avatar_url,
+    githubUrl: data.html_url,
+    name: data.name,
+    company: data.company,
+    location: data.location,
+    portfolio: data.blog,
+    email: data.email,
+    aboutMe: data.bio,
+    followers: data.followers,
+    publicRepos: data.public_repos,
+    createdAt: data.created_at,
+    updatedAt: data.updated_at,
+  };
+};
 
 module.exports = user;
