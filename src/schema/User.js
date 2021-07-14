8e5a812277a8aeb57ccb7e959f71db9dd7b6916b
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-14 17:31:03 +0100

    add User schema

---
+++ src/schema/User.js
@@ -0,0 +1,17 @@
+const User = `type User {
+  username: String!
+  profilePictureUrl: String!
+  githubUrl: String!
+  name: String!
+  company: String
+  location: String
+  portfolio: String
+  email: String
+  aboutMe: String
+  followers: Int
+  publicRepos: Int
+  createdAt: Int
+  updatedAt: Int
+}`;
+
+module.exports = User;
